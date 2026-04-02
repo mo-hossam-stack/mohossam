7288d669755099c46e2f88c0360ab6ba321b6f79
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-04-02 23:40:48 +0200

    update the number of solved problems

--- src/utils/ProjectsList.ts
+++ src/utils/ProjectsList.ts
@@ -358,8 +358,8 @@ export const ProjectsList: ProjectsListType = [
     },
     {
         Name: "LeetCode Python Journey",
-        ShortDesc: "Collection of Python solutions to +320 LeetCode problems.",
-        Desc: "A curated collection of Python solutions to +180 LeetCode problems, covering arrays, hashing, sliding window, two pointers, binary search, and more. Each file includes the problem statement, difficulty level, and a clean, well-commented solution.",
+        ShortDesc: "Collection of Python solutions to +340 LeetCode problems.",
+        Desc: "A curated collection of Python solutions to +340 LeetCode problems, covering arrays, hashing, sliding window, two pointers, binary search, and more. Each file includes the problem statement, difficulty level, and a clean, well-commented solution.",
         Logo: PythonImage,
         Shot: PythonImage,
         Mockup: PythonImage,
