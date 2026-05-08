014c1d534988cfffdbfed10ff8b66d0119d08a67
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-05-08 18:48:29 +0300

    Add ProblemSolvingJourney component and update index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Hero } from "./Hero.astro"
 export { default as About } from "./About.astro"
 export { default as Skills } from "./Skills.astro"
+export { default as ProblemSolvingJourney } from "./ProblemSolvingJourney.astro"
 export { default as Projects } from "./Projects.astro"
 export { default as Contact } from "./Contact.astro"
 export { default as Footer } from "./Footer.astro"
