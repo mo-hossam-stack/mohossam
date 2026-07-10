501596443e588e6d14e695db6525e0668c97246a
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-10 18:37:28 +0300

    update the number of solved problems

--- src/utils/ProjectsList.ts
+++ src/utils/ProjectsList.ts
@@ -456,8 +456,8 @@ export const ProjectsList: ProjectsListType = [
     },
     {
         Name: "LeetCode Python Journey",
-        ShortDesc: "Collection of Python solutions to +520 LeetCode problems.",
-        Desc: "A curated collection of Python solutions to +520 LeetCode problems, covering arrays, hashing, sliding window, two pointers, binary search, and more. Each file includes the problem statement, difficulty level, and a clean, well-commented solution.",
+        ShortDesc: "Collection of Python solutions to +550 LeetCode problems.",
+        Desc: "A curated collection of Python solutions to +550 LeetCode problems, covering arrays, hashing, sliding window, two pointers, binary search, and more. Each file includes the problem statement, difficulty level, and a clean, well-commented solution.",
         Logo: PythonImage,
         Shot: PythonImage,
         Mockup: PythonImage,
